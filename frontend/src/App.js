1c3593b863e750ec58723ef396e76a732d96fb30
sourabhshukla <48114727+sourabhshukla@users.noreply.github.com>
2023-04-16 05:17:39 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,8 @@ import Thanks from "./components/Thanks";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/v1`,
+  //endpoint: `http://${ipConfig.workspaceIp}:8082/v1`,
+  endpoint: 'https://qkart-backend2-y82i.onrender.com/v1'
 };
 
 export default function App(props) {
